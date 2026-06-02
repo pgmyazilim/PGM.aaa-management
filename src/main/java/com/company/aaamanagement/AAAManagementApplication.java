644c91e5da2a16fcfcdaccbee28d0a4914d85b5b
oguzhan <oguzhan_kobe@hotmail.com>
2026-06-02 13:50:00 +0300

    chore: initial project setup

---
+++ src/main/java/com/company/aaamanagement/AAAManagementApplication.java
@@ -0,0 +1,11 @@
+package com.company.aaamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AAAManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AAAManagementApplication.class, args);
+    }
+}
